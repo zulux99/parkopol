ecae4d362a358c2664701f44c5a7634bd062d6e3
BuildTools <unconfigured@null.spigotmc.org>
2021-05-09 22:54:22 +0200

    jb300

--- app/src/main/java/com/example/parkopol/SecondActivity.kt
+++ app/src/main/java/com/example/parkopol/SecondActivity.kt
@@ -1,9 +1,13 @@
 package com.example.parkopol
 
-import android.R
+import android.content.Intent
 import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
@@ -11,14 +15,21 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.example.parkopol.databinding.ActivityMainBinding.bind
 import com.example.parkopol.databinding.ActivitySecondBinding
+import com.example.parkopol.databinding.ActivitySecondBinding.bind
 import com.example.parkopol.databinding.NavHeaderBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import java.io.IOException
 import java.io.InputStream
 import java.net.MalformedURLException
 import java.net.URL
+import com.example.parkopol.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class SecondActivity : AppCompatActivity() {
@@ -29,26 +40,39 @@ class SecondActivity : AppCompatActivity() {
         binding = ActivitySecondBinding.inflate(layoutInflater)
         val headerBinding = NavHeaderBinding.bind(binding.navView.getHeaderView(0))
         setContentView(binding.root)
-        Toast.makeText(this, "Zalogowano", Toast.LENGTH_LONG).show()
         val signInAccount: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(this)
         drawer = binding.drawerLayout
         val toogle = ActionBarDrawerToggle(this, drawer, binding.toolbar,
-            R.string.unknownName, R.string.unknownName)
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer.addDrawerListener(toogle)
         toogle.syncState()
         if(signInAccount != null){
             headerBinding.imieNazwisko.text = signInAccount.displayName
             headerBinding.adresEmail.text = signInAccount.email
-        }
-//    jb300
 
-//
+        }
 //        binding.wyloguj.setOnClickListener {
-//            Firebase.auth.signOut()
-//            val intent = Intent(this, MainActivity::class.java)
-//            startActivity(intent)
-//            Toast.makeText(this, "Wylogowano", Toast.LENGTH_LONG).show()
+//
 //        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        val inflater = menuInflater
+        inflater.inflate(R.menu.lewy_sidebar,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Toast.makeText(this, "Wylogowano", Toast.LENGTH_LONG).show()
+        when (item.itemId) {
+            R.id.nav_logout -> {
+                Firebase.auth.signOut()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
     override fun onBackPressed(){
         if (drawer.isDrawerOpen(GravityCompat.START)){
