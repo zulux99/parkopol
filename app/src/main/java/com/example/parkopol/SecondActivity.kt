cbbcc52132b52aef6b92f9ddae96ae4384f53973
BuildTools <unconfigured@null.spigotmc.org>
2021-05-10 18:32:45 +0200

    działające navView

--- app/src/main/java/com/example/parkopol/SecondActivity.kt
+++ app/src/main/java/com/example/parkopol/SecondActivity.kt
@@ -1,38 +1,27 @@
 package com.example.parkopol
 
 import android.content.Intent
-import android.graphics.BitmapFactory
-import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import android.widget.ImageView
 import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import com.example.parkopol.databinding.ActivityMainBinding.bind
 import com.example.parkopol.databinding.ActivitySecondBinding
-import com.example.parkopol.databinding.ActivitySecondBinding.bind
 import com.example.parkopol.databinding.NavHeaderBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import java.io.IOException
-import java.io.InputStream
-import java.net.MalformedURLException
-import java.net.URL
-import com.example.parkopol.R
-import com.google.firebase.auth.FirebaseAuth
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 
-class SecondActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var drawer: DrawerLayout
     private lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,43 +31,48 @@ class SecondActivity : AppCompatActivity() {
         setContentView(binding.root)
         val signInAccount: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(this)
         drawer = binding.drawerLayout
-        val toogle = ActionBarDrawerToggle(this, drawer, binding.toolbar,
+        val toogle = ActionBarDrawerToggle(
+            this, drawer, binding.toolbar,
             R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer.addDrawerListener(toogle)
         toogle.syncState()
-        if(signInAccount != null){
+        if (signInAccount != null) {
             headerBinding.imieNazwisko.text = signInAccount.displayName
             headerBinding.adresEmail.text = signInAccount.email
-
         }
-//        binding.wyloguj.setOnClickListener {
-//
-//        }
-    }
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        super.onCreateOptionsMenu(menu)
-        val inflater = menuInflater
-        inflater.inflate(R.menu.lewy_sidebar,menu)
-        return super.onCreateOptionsMenu(menu)
+        val navigationView = binding.navView
+        navigationView.bringToFront()
+        navigationView.setNavigationItemSelectedListener(this)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        Toast.makeText(this, "Wylogowano", Toast.LENGTH_LONG).show()
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.nav_logout -> {
+                Toast.makeText(this, "Wylogowano", Toast.LENGTH_LONG).show()
                 Firebase.auth.signOut()
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 return true
             }
-        }
-        return super.onOptionsItemSelected(item)
-    }
-    override fun onBackPressed(){
-        if (drawer.isDrawerOpen(GravityCompat.START)){
-            drawer.closeDrawer(GravityCompat.START)
-        } else{
-            super.onBackPressed()
+            R.id.nav_konto -> {
+
+            }
+            R.id.nav_historia -> {
+
+            }
+            R.id.nav_oplac -> {
+
+            }
+            else -> return super.onOptionsItemSelected(item)
         }
     }
 }
+
