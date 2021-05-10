c232be4a712b98f8f61624bf30b42c96b5681781
BuildTools <unconfigured@null.spigotmc.org>
2021-05-10 23:44:27 +0200

    działające Fragmenty Konto i Historia, przechodzenie do innych layoutów z NavView

--- app/src/main/java/com/example/parkopol/SecondActivity.kt
+++ app/src/main/java/com/example/parkopol/SecondActivity.kt
@@ -3,6 +3,7 @@ package com.example.parkopol
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -10,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.parkopol.databinding.ActivitySecondBinding
+import com.example.parkopol.databinding.FragmentKontoBinding
 import com.example.parkopol.databinding.NavHeaderBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -22,17 +24,18 @@ import com.squareup.picasso.Picasso
 class SecondActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var drawer: DrawerLayout
     private lateinit var binding: ActivitySecondBinding
+    private lateinit var fragmentKontoBinding: FragmentKontoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondBinding.inflate(layoutInflater)
         val headerBinding = NavHeaderBinding.bind(binding.navView.getHeaderView(0))
+        fragmentKontoBinding = FragmentKontoBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val signInAccount: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(this)
         drawer = binding.drawerLayout
         val toogle = ActionBarDrawerToggle(
             this, drawer, binding.toolbar,
-            R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        )
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer.addDrawerListener(toogle)
         toogle.syncState()
         if (signInAccount != null) {
@@ -44,8 +47,10 @@ class SecondActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
         val navigationView = binding.navView
         navigationView.bringToFront()
         navigationView.setNavigationItemSelectedListener(this)
+//        KTÓRY FRAGMENT MA SIĘ WYŚWIETLAĆ NA STARCIE
+//        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, KontoFragment()).commit()
+//        navigationView.setCheckedItem(R.id.nav_konto)
     }
-
     override fun onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START)
@@ -53,22 +58,24 @@ class SecondActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
             super.onBackPressed()
         }
     }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.nav_logout -> {
-                Toast.makeText(this, "Wylogowano", Toast.LENGTH_LONG).show()
                 Firebase.auth.signOut()
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
+                Toast.makeText(this, "Wylogowano", Toast.LENGTH_LONG).show()
+                drawer.closeDrawer(GravityCompat.START)
                 return true
             }
             R.id.nav_konto -> {
-
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, KontoFragment()).commit()
+                drawer.closeDrawer(GravityCompat.START)
                 return true
             }
             R.id.nav_historia -> {
-
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HistoriaFragment()).commit()
+                drawer.closeDrawer(GravityCompat.START)
                 return true
             }
             R.id.nav_oplac -> {
