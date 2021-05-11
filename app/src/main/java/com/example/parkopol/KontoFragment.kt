4cb79ae648099dceb0ec843b7fa6482f50f95b14
Iidontknown <jakub-strugala@wp.pl>
2021-05-11 08:58:04 +0200

    cos

--- app/src/main/java/com/example/parkopol/KontoFragment.kt
+++ app/src/main/java/com/example/parkopol/KontoFragment.kt
@@ -1,9 +1,13 @@
 package com.example.parkopol
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class KontoFragment: Fragment() {
     override fun onCreateView(
@@ -13,4 +17,20 @@ class KontoFragment: Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_konto, container, false)
     }
+    private fun deleteUser() {
+        // [START delete_user]
+        val user = Firebase.auth.currentUser!!
+
+        user.delete()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "User account deleted.")
+                }
+            }
+        // [END delete_user]
+    }
+
+
+
+
 }
