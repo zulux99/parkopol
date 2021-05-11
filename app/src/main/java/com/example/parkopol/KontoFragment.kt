339c77e92c9eaa48ac514cf5ccd80b572dc7cddf
Iidontknown <jakub-strugala@wp.pl>
2021-05-11 09:20:24 +0200

    cos

--- app/src/main/java/com/example/parkopol/KontoFragment.kt
+++ app/src/main/java/com/example/parkopol/KontoFragment.kt
@@ -6,31 +6,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.parkopol.databinding.FragmentKontoBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class KontoFragment: Fragment() {
+    private lateinit var binding: FragmentKontoBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
+        binding = FragmentKontoBinding.inflate(layoutInflater)
+        binding.kontoUsun.setOnClickListener{
+            deleteUser()
+            Log.d("komunikat", "1")
+        }
         return inflater.inflate(R.layout.fragment_konto, container, false)
     }
+
     private fun deleteUser() {
+        Log.d("komunikat", "odp")
         // [START delete_user]
         val user = Firebase.auth.currentUser!!
 
         user.delete()
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    Log.d(TAG, "User account deleted.")
+                    Log.d("komunikat", "User account deleted.")
                 }
             }
         // [END delete_user]
     }
-
-
 
 
 }
