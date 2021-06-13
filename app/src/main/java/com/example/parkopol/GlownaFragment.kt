4b84e1a53b922aa9aafa0b43a490303d4b46d164
Iidontknown <jakub-strugala@wp.pl>
2021-06-13 16:51:17 +0200

    ostanie poprawki

--- app/src/main/java/com/example/parkopol/GlownaFragment.kt
+++ app/src/main/java/com/example/parkopol/GlownaFragment.kt
@@ -1,8 +1,10 @@
 package com.example.parkopol
 
 import android.content.Intent
+import android.media.Image
 import android.os.Bundle
 import android.os.Handler
+import android.system.Os.bind
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -10,13 +12,20 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import com.example.parkopol.databinding.ActivityMainBinding.bind
+import com.example.parkopol.databinding.ActivitySecondBinding
+import com.example.parkopol.databinding.FragmentGlownaBinding
+import com.example.parkopol.databinding.FragmentKontoBinding
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
 
 class GlownaFragment : Fragment() {
+    private lateinit var glownaBinding: FragmentGlownaBinding
+    private lateinit var secondBinding: ActivitySecondBinding
     var listaLokalizacji = ArrayList<DodajParkingFragment.MiejsceParkingowe>()
     private var listaZaparkowan = ArrayList<Zaparkowanie>()
     override fun onCreateView(
@@ -30,9 +39,13 @@ class GlownaFragment : Fragment() {
         Log.d("glowna", "lista: $listaZaparkowan")
         val myView = inflater.inflate(R.layout.fragment_glowna, container, false)
         val zakonczParkowanieButton = myView.findViewById(R.id.secondZakonczZaparkowanie) as Button
-
+        glownaBinding = FragmentGlownaBinding.bind(myView)
+        secondBinding = ActivitySecondBinding.inflate(layoutInflater)
         val secondCzasZaparkowaniaTextView = myView.findViewById(R.id.secondCzasZaparkowania) as TextView
         val secondZaparkowanieOpisTextView = myView.findViewById(R.id.secondZaparkowanieOpis) as TextView
+        val glownaZaparkujButton = myView.findViewById(R.id.glownaZaparkuj) as Button
+
+        val textView9 = myView.findViewById(R.id.textView9) as TextView
         val idMP="";
         var startZaparkowanie=""
 
@@ -48,6 +61,10 @@ class GlownaFragment : Fragment() {
                     var opis= listaLokalizacji.findLast { it.idMParkingowego== tempczas.idMiejsceParkingowe}
                     if (opis!=null){
                         secondZaparkowanieOpisTextView.setText(opis.opis)
+                        secondZaparkowanieOpisTextView.visibility = View.VISIBLE
+                        secondZaparkowanieOpisTextView.visibility = View.VISIBLE
+                        glownaBinding.glownaLogo.visibility = View.GONE
+                        glownaZaparkujButton.visibility=View.GONE
                     }else{
                         secondZaparkowanieOpisTextView.setText("Brak informacji")
                     }
@@ -61,9 +78,11 @@ class GlownaFragment : Fragment() {
                     val a = formatter.parse(SimpleDateFormat("yyyy-MM-dd HH:mm:ss",
                         Locale.GERMAN).format(Date()))
                     val b = formatter.parse(tempczas.startZaparkowania)
-                    val tem =b!!.getTime() - a!!.getTime()
+                    val tem =a!!.getTime() - b!!.getTime()
                     x=roznicaCzas(tem)
-
+                    secondCzasZaparkowaniaTextView.visibility = View.VISIBLE
+                    textView9.visibility = View.VISIBLE
+                    zakonczParkowanieButton.visibility = View.VISIBLE
                 }
 
 
@@ -106,7 +125,25 @@ class GlownaFragment : Fragment() {
                 Toast.makeText(context, "nie zaparkowałeś", Toast.LENGTH_SHORT).show()
             }
         }
+        glownaZaparkujButton.setOnClickListener()
+        {
+            listaZaparkowan = tablicaZaparkowanie(listaZaparkowan)
 
+            Log.d("komunkat", "rozmiar"+ listaZaparkowan.size.toString()+" id:"+listaZaparkowan.findLast {it.koniecZaparkowania == "0"  }?.idZap)
+            if (!(listaZaparkowan.any { it.koniecZaparkowania == "0" })){
+
+                if (activity != null) {
+                    activity!!.supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, MapaFragment()).commit()
+                    val navigationView = secondBinding.navView
+                    navigationView.setCheckedItem(R.id.nav_mapa)// TODO: nie zaznacza mapy i chuj
+//            navigationView.menu.findItem(R.id.nav_mapa).isChecked = true// TODO: w ten sposób też nie aaaaaaaaaaaaa}
+                }
+                listaLokalizacji = tablicaMiejscaParkingowebaza(listaLokalizacji)
+            } else {
+                Toast.makeText(context, "Zaparkowałeś już", Toast.LENGTH_SHORT).show()
+            }
+        }
         return myView
     }
 
